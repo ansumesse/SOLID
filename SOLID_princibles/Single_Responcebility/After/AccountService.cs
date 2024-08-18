d7d93c9ffac5be977088997aa2522548f41c5021
m-anass <manas8269@gmail.com>
2024-08-18 23:06:11 +0300

    Single Responsibility Solid

---
+++ SOLID_princibles/Single_Responcebility/After/AccountService.cs
@@ -0,0 +1,52 @@
+﻿using SOLID.SRP.Before;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility.After
+{
+    internal class AccountService
+    {
+        public void Withdraw(decimal amount, Account account)
+        {
+            var transactionMessage = "";
+
+            // handle withdraw
+            if (amount > 0)
+            {
+                if (account.Balance < Math.Abs(amount))
+                {
+                    transactionMessage =
+                    $"OVERDRAFT when trying to withdraw " +
+                    $"{Math.Abs(amount).ToString("C2")}," +
+                    $" current balance {account.Balance.ToString("C2")}";
+                }
+                else
+                {
+                    account.Balance -= amount;
+                    transactionMessage =
+                       $"OK Withdraw {Math.Abs(amount).ToString("C2")}" +
+                       $", current balance {account.Balance.ToString("C2")}";
+                }
+                EmailService emailService = new EmailService();
+                emailService.Send(account, transactionMessage);
+            }
+        }
+        public void Deposite(decimal amount, Account account)
+        {
+            var transactionMessage = "";
+            if (amount > 0)
+            {
+                account.Balance += amount;
+
+                transactionMessage =
+                    $"OK Deposit {amount.ToString("C2")}" +
+                    $", current balance {account.Balance.ToString("C2")}";
+            }
+            EmailService emailService = new EmailService();
+            emailService.Send(account, transactionMessage);
+        }
+    }
+}
--- SOLID_princibles/Single_Responcebility/Program.cs
+++ SOLID_princibles/Single_Responcebility/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Single_Responcebility
+﻿using Single_Responsibility.After;
+
+namespace Single_Responsibility
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Account account = new Account("Mohamed", "mohamed@gamil.com", 30000);
+            AccountService accountService = new AccountService();
+            accountService.Withdraw(5000, account);
+            accountService.Deposite(5000, account);
+
         }
     }
 }
